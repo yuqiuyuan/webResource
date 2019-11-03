f0f7d1bb97af5bc9dea872ad6c95b333fc544e4b
yuqiuyuan <derbander@163.com>
2019-11-03 09:31:10 +0800

    Initial commit

---
+++ src/main/java/com/drebander/webResource/WebResourceApplication.java
@@ -0,0 +1,13 @@
+package com.drebander.webResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drebander/webResource/WebResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drebander.webResource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
